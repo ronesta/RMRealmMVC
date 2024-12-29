8c2bc2d38244ccd1439014f5f0795e5437ed7e64
ronesta <gvozzzd123@gmail.com>
2024-12-29 15:29:39 +0300

    Initial commit

---
+++ RMRealmMVC/RMRealmMVC/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RMRealmMVC
+//
+//  Created by Ибрагим Габибли on 29.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
